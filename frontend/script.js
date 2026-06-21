d83ba97c479de0899ad413209ab0a0cc7c5939f4
madesh-zre <m66736681@gmail.com>
2026-06-21 20:02:07 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,8 @@
+function previewImage(event) {
+    let image = document.getElementById("preview");
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image.style.display = "block";
+}
 function analyzeFood() {
     let weight = document.getElementById("weight").value;
 
