b0a7173ba673e3db07c8c5957f9304a8f9254f11
madesh-zre <m66736681@gmail.com>
2026-06-21 19:57:30 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,15 @@
+function analyzeFood() {
+    let weight = document.getElementById("weight").value;
 
+    if (!weight) {
+        alert("Please enter food weight");
+        return;
+    }
+
+    // Dummy data for now (we will replace with AI later)
+    document.getElementById("foodName").innerText = "Chicken Rice";
+    document.getElementById("calories").innerText = weight * 2.5;
+    document.getElementById("protein").innerText = weight * 0.1;
+    document.getElementById("carbs").innerText = weight * 0.3;
+    document.getElementById("fat").innerText = weight * 0.05;
+}
